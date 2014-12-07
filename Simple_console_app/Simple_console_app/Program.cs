acb52c8b43dba08aee24179edaf3b736ff216735
Alex <alex@alex.lv>
2014-12-07 17:45:46 +0200

    pievienota izvade: x un y summa ir ...

--- Simple_console_app/Simple_console_app/Program.cs
+++ Simple_console_app/Simple_console_app/Program.cs
@@ -17,7 +17,7 @@ namespace Simple_console_app
             Console.WriteLine("Ievadi x: "); s = Console.ReadLine(); x = int.Parse(s);
             Console.WriteLine("Ievadi y: "); s = Console.ReadLine(); y = int.Parse(s);
             w = x + y;
-
+            Console.WriteLine("x un y summa ir: {0}", w);
             Console.ReadLine();
         }
     }
