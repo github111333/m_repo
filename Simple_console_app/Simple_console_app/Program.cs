ba23912033fe81b741c6fe1bac1117ef9e5c3e1e
Alex <alex@alex.lv>
2014-12-07 16:53:26 +0200

    vienkarsa konsoles aplikacijas izveide

---
+++ Simple_console_app/Simple_console_app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is simple Console App.");
+            Console.ReadLine();
+        }
+    }
+}
